e5151cfafef903e86760440286c51a377276da9d
Jadie <117045702+Karllr@users.noreply.github.com>
2024-12-23 13:32:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-anime({
-    targets:'#item1',
-    keyframes:[
-        {translateX:-100},{translateX:100},{translateX:-100}
-    ],
-    opacity:1,
-    easing:'easeInOutExpo',
-    duration:2000,
-    direction:'alternate',
-    loop:true
-})
-anime({
-    targets:'#item2',
-    //translateX:100,
-    keyframes:[
-        {translateX:100},{translateX:-100},{translateX:100}
-    ],
-    easing:'easeInOutExpo',
-    duration:2000,
-    direction:'alternate',
-    loop:true
-});
-anime({
-    targets:'.turnTable',
-    duration:20000,
-    easing:'linear',
-    translateX:3450,
-    loop:true
-});
+const sections=document.getElementsByTagName("section");
+
+//document.body.style.height=`${sections[0].getBoundingClientRect().height*(sections.length+1)}px`;
+
+
+window.onscroll=(e)=>{
+    var scrolly=document.body.getBoundingClientRect().y;
+    //console.log(window.getBoundingClientRect())
+    for(var i=0;i<sections.length;i++){
+        //sections[i].style.transform=`translate(${scrolly}px,0px)`;
+        sections[i].animate(
+            {
+                transform:`translate(${scrolly}px,0px)`
+            },
+            {
+                duration:500,
+                easing:"linear",
+                fill:"forwards"
+            }
+        )
+    }
+}
